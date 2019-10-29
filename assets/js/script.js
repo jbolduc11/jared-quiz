2ee7feaf94b3b120b8b56562a6e7491972dce692
Benjamin Russell <eysikal@gmail.com>
2019-10-29 01:55:20 -0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+function gradeQuiz(event) {
+    event.preventDefault(); // prevent actual form submission and page reload
+
+    const submitBtn = document.querySelector('button');
+    submitBtn.addEventListener('click', () => {location.reload(true)}); // reload page
+    submitBtn.innerText = 'Start Over';
+
+    const answerKey  = ['c', 'c', 'a', 'a', 'b'],
+          answers = document.querySelectorAll('input:checked');
+    let score = 0;
+    for (var i = 0; i < answers.length; i++) {
+        if (answers[i].value !== answerKey[i]) {
+            answers[i].parentElement.className = 'incorrect';
+        } else {
+            score++;
+        }
+    }
+
+    let begin = '',
+        compare = '';
+
+    switch (score) {
+        case 0:
+            begin = 'Ouch!';
+            compare = 'We have some work to do';
+            break;
+        case 1:
+            begin = 'Great Effort!';
+            compare = 'Below Average';
+            break;
+        case 2:
+            begin = 'Great!';
+            compare = 'Average';
+            break;
+        case 3:
+            begin = 'Great!';
+            compare = 'Average';
+            break;
+        case 4:
+            begin = 'Well done!';
+            compare = 'Above Average';
+            break;
+        case 5:
+            begin = 'Well done!';
+            compare = 'Above Average';
+            break;
+    } 
+        
+    const results = document.getElementById('results');
+    results.innerHTML = `${begin} you've completed your financial knowledge assessment! You got <strong>${score}/5</strong> correct which means your financial knowledge is ${compare}`;
+    results.style.display = 'block';
+}
